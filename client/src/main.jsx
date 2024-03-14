ad93f8b5b297fb6c34603392abac9770804011af
rlobz <rlobzowski@gmail.com>
2024-03-14 18:46:31 +0000

    done main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,27 @@
 import './css/index.css';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import SearchAnime from './pages/SearchAnime';
+import SavedAnime from './pages/SavedAnime';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='text-4xl text-center'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchAnime />
+      }, {
+        path: '/saved',
+        element: <SavedAnime />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
