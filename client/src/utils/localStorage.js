7fc31acb9ac22f8fd6ac12e92a63ab13860751c6
rlobz <rlobzowski@gmail.com>
2024-03-17 14:08:56 +0000

    fixed export

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,25 +1,24 @@
-const getSavedAnimeIds = () => {
-    const savedAnimeIds = localStorage.getItem('saved_animes')
-      ? JSON.parse(localStorage.getItem('saved_animes'))
-      : [];
-    return savedAnimeIds;
-  };
-  export const saveAnimeIds = (animeIdArr) => {
-    if (animeIdArr.length) {
-      localStorage.setItem('saved_animes', JSON.stringify(animeIdArr));
-    } else {
-      localStorage.removeItem('saved_animes');
-    }
-  };
-  export const removeAnimeId = (animeId) => {
-    const savedAnimeIds = localStorage.getItem('saved_animes')
-      ? JSON.parse(localStorage.getItem('saved_animes'))
-      : null;
-    if (!savedAnimeIds) {
-      return false;
-    }
-    const updatedSavedAnimeIds = savedAnimeIds?.filter((savedAnimeId) => savedAnimeId !== animeId);
-    localStorage.setItem('saved_animes', JSON.stringify(updatedSavedAnimeIds));
-    return true;
-  };
-  
+export const getSavedAnimeIds = () => {
+  const savedAnimeIds = localStorage.getItem('saved_animes')
+    ? JSON.parse(localStorage.getItem('saved_animes'))
+    : [];
+  return savedAnimeIds;
+};
+export const saveAnimeIds = (animeIdArr) => {
+  if (animeIdArr.length) {
+    localStorage.setItem('saved_animes', JSON.stringify(animeIdArr));
+  } else {
+    localStorage.removeItem('saved_animes');
+  }
+};
+export const removeAnimeId = (animeId) => {
+  const savedAnimeIds = localStorage.getItem('saved_animes')
+    ? JSON.parse(localStorage.getItem('saved_animes'))
+    : null;
+  if (!savedAnimeIds) {
+    return false;
+  }
+  const updatedSavedAnimeIds = savedAnimeIds?.filter((savedAnimeId) => savedAnimeId !== animeId);
+  localStorage.setItem('saved_animes', JSON.stringify(updatedSavedAnimeIds));
+  return true;
+};
