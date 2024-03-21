43ee28809aea4cfa7f735a6a3c5b2e45cfc317b0
rlobz <rlobzowski@gmail.com>
2024-03-21 20:22:29 +0000

    env new

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,4 +4,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection; 
